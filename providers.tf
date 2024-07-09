07c2ad36d3a4bfd4ec7645a5359e988490ea4040
ASIM SALIMLI <34362280+civilizador@users.noreply.github.com>
2024-07-09 11:59:57 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -20,6 +20,7 @@ provider "google" {
 }
 
 provider "wiz" {
+  alias                  = "default"
   wiz_url                = var.wiz_url
   wiz_auth_client_id     = var.wiz_auth_client_id
   wiz_auth_client_secret = var.wiz_auth_client_secret
