b7e65088aaaa9e3f49529b7e8974b607d2175a77
ASIM SALIMLI <34362280+civilizador@users.noreply.github.com>
2024-07-09 12:26:51 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -7,8 +7,8 @@ terraform {
       version = ">= 3.50.0"  # Specify the version constraint for the Google provider
     }
     wiz = {
-      source = "hashicorp/wiz"
-      version = ">=1.0.0"
+      source  = "AxtonGrams/wiz"
+      version = "1.0.2"
     }
   }
 }
